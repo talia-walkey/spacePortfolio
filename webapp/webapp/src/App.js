c03e851230bc22a3753bc373161c85a1f12032c5
Alynna Alcira <alynna.alcira@gmail.com>
2018-01-22 16:39:33 -0800

    Finally done

--- webapp/webapp/src/App.js
+++ webapp/webapp/src/App.js
@@ -17,15 +17,23 @@ class App extends Component {
                 <div id="welcome" class="nameHeading" id="welcomeDiv">Welcome to our landing page!</div>
             </div>
 
+            <div id="images">
+                <img src={require('./Image/AlynnaImg.png')} id="aimg" />
+                <img src={require('./Image/TaliaImg.png')} id="timg" />
+            </div>
+
         <div id="tbioBkg">
-                Talia Walkey <br/>
-                I am a marker, developer, and designer with a Digital Design and Development Diploma from British Columbia Institute of Technology! Not only am I extremely diligent, organized, and focused, I am comfortable pitching myself, my company, and my product. I have skills in everything from design to development of web and mobile applications as well as I have learned the business skills to market them. I am proficient with front-end, and back-end development, along with Adobe Creative Suite. 
+                I am a marker, developer, and designer with a Digital Design and Development Diploma from British Columbia Institute of Technology! I have skills in everything from design to development of web and mobile applications as well as I have learned the business skills to market them. 
         </div>
+                <h1 id="tname">Talia Walkey</h1>
+                <img src={require('./Image/speechleft.png')} id="speechleft" />
         <br/>
         <div id="abioBkg">
-                Alynna Alcira <br/>
-                Hello! I am an aspiring UX/UI designer currently studying Digital Design and Development at BCIT. I am a creative and quality driven individual, motivated to create new and innovating user experiences. I love to bring my ideas to life, either through graphics, videos, and/or websites.
+                Hello! I am an aspiring UX/UI designer currently studying Digital Design and Development at BCIT. I am a creative and quality driven individual, motivated to create new user experiences. I love to bring my ideas to life, either through graphics and/or websites.
         </div>
+                <h1 id="aname">Alynna Alcira</h1>
+
+                <img src={require('./Image/speechright.png')} id="speechright" />
       </div>
     );
   }
